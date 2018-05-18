fa934715e7bb94a66ff996a438fa52ed1203acb1
Jinchi Zhou <jinchizhou@yahoo.com>
2018-05-18 15:21:25 -0700

    still debugging

--- matmul.cpp
+++ matmul.cpp
@@ -19,24 +19,29 @@ void matmul( float*, const float*, const float*, unsigned int, unsigned int, uns
 void matmul(float* C, const float* A, const float* B, unsigned int hA, 
     unsigned int wA, unsigned int wB)
 {
-  for (unsigned int i = 0; i < hA; ++i)  {
+  /*for (unsigned int i = 0; i < hA; ++i)  {
     for (unsigned int j = 0; j < wB; ++j) {
       double sum = 0;
       for (unsigned int k = 0; k < wA; ++k) {
         double a = A[i * wA + k];
         double b = B[k * wB + j];
-        /*double c = B[(k+1) * wB + j];
-        sum1 = a * b
-        sum2 = a * c
-        sum += sum1 + sum2
-        */
         sum += a * b;
       }
       C[i * wB + j] = (float)sum;
-      /*C[i * wB + j] = (float)sum1;
-      C[i * wB + j + 1] = (float)sum2; */
+      C[i * wB + j] = (float)sum1;
+      C[i * wB + j + 1] = (float)sum2; 
     }
   } 
+*/
+  for (unsigned int r = 0; r < hA; r++) {
+    for (unsigned int c = 0; c < wB; c++) {
+      //double sum1 = 0;
+      //double sum2 = 0;
+      for (unsigned int k = 0; k < wA; k++) {
+        C[r*hA+k] += float(A[r*hA+c])*float(B[k*hA+c]);
+      }
+    }
+  }
   /*for (unsigned int i = 0; i < hA; ++i)  {
     for (unsigned int j = 0; j < hA; ++j) {
       double sum = 0;
@@ -75,15 +80,15 @@ Matrix Allocate2ndMatrix(int height, int width)
    * You need to modify this function which is only called
    * for Matrix B such that a column-major ordering is
    * performed. */
-  /*for(unsigned int i = 0; i < M.height; i++){
+  for(unsigned int i = 0; i < M.height; i++){
     for(unsigned int j = 0; j < M.width; j++){
       M.elements[i+j * M.width] = (rand() / (float)RAND_MAX);
     }
-  } */
-  for(unsigned int i = 0; i < M.height * M.width; i++)
+  }
+  /*for(unsigned int i = 0; i < M.height * M.width; i++)
   {
     M.elements[i] = (rand() / (float)RAND_MAX);
-  }
+  }*/
   return M;
 
 }
---
+++ tests/tester.cpp
@@ -0,0 +1,26 @@
+/*#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+// includes, kernels
+#include "matmul.h"
+
+
+void matmul(float*, const float*, const float*, unsigned int, unsigned int, unsigned int);
+*/
+/*int mabain() {
+  Matrix M;
+  Matrix N;
+  Matrix P;
+  for (int a = 0; a < 16; a++){
+    M.elements[a] = a + 1;
+  }
+  for (int a = 0; a < 16; a++){
+    N.elements[a] = 16 - a;
+  }
+  matmul(P.elements, M.elements, N.elements, 4, 4, 4);
+  for (int a = 0; a < 16; a++){
+    fprintf(stdout, "%d", int(P.elements[a]));
+  }
+}*/
