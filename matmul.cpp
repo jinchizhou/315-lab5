04cd2366f9947679ff7400763a24e0bccc65b201
Jinchi Zhou <jinchizhou@yahoo.com>
2018-05-18 00:05:19 -0700

    done with f

--- matmul.cpp
+++ matmul.cpp
@@ -19,16 +19,45 @@ void matmul( float*, const float*, const float*, unsigned int, unsigned int, uns
 void matmul(float* C, const float* A, const float* B, unsigned int hA, 
     unsigned int wA, unsigned int wB)
 {
-  for (unsigned int i = 0; i < hA; ++i)
+  for (unsigned int i = 0; i < hA; ++i)  {
     for (unsigned int j = 0; j < wB; ++j) {
       double sum = 0;
       for (unsigned int k = 0; k < wA; ++k) {
         double a = A[i * wA + k];
         double b = B[k * wB + j];
+        /*double c = B[(k+1) * wB + j];
+        sum1 = a * b
+        sum2 = a * c
+        sum += sum1 + sum2
+        */
+        sum += a * b;
+      }
+      C[i * wB + j] = (float)sum;
+      /*C[i * wB + j] = (float)sum1;
+      C[i * wB + j + 1] = (float)sum2; */
+    }
+  } 
+  /*for (unsigned int i = 0; i < hA; ++i)  {
+    for (unsigned int j = 0; j < hA; ++j) {
+      double sum = 0;
+      for (unsigned int k = 0; k < wA; ++k) {
+        double a = A[i * wA + k];
+        double b = B[j * wB + k];
         sum += a * b;
       }
       C[i * wB + j] = (float)sum;
     }
+  }*/
+  /*for (unsigned int row = 0; row < hA; row++) {
+    unsigned l = 0;
+    unsigned sum = 0;
+    for (unsigned int r = 0; r < wB * wB; r++) {
+      sum += A[] * B[r];
+      l++;
+      l%=wB;
+    }
+  }
+  */
 }
 
 // Allocate a matrix of dimensions height*width
@@ -46,10 +75,16 @@ Matrix Allocate2ndMatrix(int height, int width)
    * You need to modify this function which is only called
    * for Matrix B such that a column-major ordering is
    * performed. */
+  /*for(unsigned int i = 0; i < M.height; i++){
+    for(unsigned int j = 0; j < M.width; j++){
+      M.elements[i+j * M.width] = (rand() / (float)RAND_MAX);
+    }
+  } */
   for(unsigned int i = 0; i < M.height * M.width; i++)
   {
     M.elements[i] = (rand() / (float)RAND_MAX);
   }
   return M;
-}	
+
+}
 
