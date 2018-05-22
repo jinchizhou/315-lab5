21a5a01f473bfc5423f5c3bbf8f7b14e95caeb28
Jinchi Zhou <jinchizhou@yahoo.com>
2018-05-22 10:16:29 -0700

    m

--- matmul.cpp
+++ matmul.cpp
@@ -38,6 +38,7 @@ void matmul(float* C, const float* A, const float* B, unsigned int hA,
       double sum3 = 0;
       double sum4 = 0;
       for (unsigned int k = 0; k < wA; ++k) {
+        
       	int temp1 = i * wA + k;
       	int temp2 = (i+1) * wA + k;
       	int temp3 = j * wB + k;
@@ -78,9 +79,9 @@ void matmul(float* C, const float* A, const float* B, unsigned int hA,
     }
   }
   */
-   /*
-  for (unsigned int i = 0; i < hA; i += 4)  {
-    for (unsigned int j = 0; j < hA; j += 4) {
+  /*
+  for (unsigned int i = 0; i < hA; ++i)  {
+    for (unsigned int j = 0; j < hA; ++j) {
       double sum1 = 0;
       double sum2 = 0;
       double sum3 = 0;
